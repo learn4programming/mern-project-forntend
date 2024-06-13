f7e8ca6f07146c5eb8f14205d6a70ccc23f34004
Chun Jung <rong820406@gmail.com>
2024-06-13 13:12:47 +0800

    frontend update

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Layout from "./components/Layout";
+import HomeComponent from "./components/home-component";
+import RegisterComponent from "./components/register-component";
+import LoginComponent from "./components/login-component";
+import ProfileComponent from "./components/profile-component";
+import AuthServices from "./services/auth.services";
+import CourseComponent from "./components/course-component";
+import PostCourseComponent from "./components/postCourse-component";
+import EnrollComponent from "./components/enroll-component";
+
+function App() {
+  let [currentUser, setCurrentUser] = useState(AuthServices.getCurrentUser());
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout currentUser={currentUser} setCurrentUser={setCurrentUser} />
+          }
+        >
+          <Route index element={<HomeComponent />} />
+          <Route path="register" element={<RegisterComponent />} />
+          <Route
+            path="login"
+            element={
+              <LoginComponent
+                urrentUser={currentUser}
+                setCurrentUser={setCurrentUser}
+              />
+            }
+          />
+
+          <Route
+            path="profile"
+            element={
+              <ProfileComponent
+                currentUser={currentUser}
+                setCurrentUser={setCurrentUser}
+              />
+            }
+          />
+
+          <Route
+            path="course"
+            element={
+              <CourseComponent
+                currentUser={currentUser}
+                setCurrentUser={setCurrentUser}
+              />
+            }
+          />
+
+          <Route
+            path="postCourse"
+            element={
+              <PostCourseComponent
+                currentUser={currentUser}
+                setCurrentUser={setCurrentUser}
+              />
+            }
+          />
+
+          <Route
+            path="enroll"
+            element={
+              <EnrollComponent
+                currentUser={currentUser}
+                setCurrentUser={setCurrentUser}
+              />
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
