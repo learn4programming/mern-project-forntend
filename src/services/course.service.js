199e2edf8667eea703a72eb377d68f5855680574
Chun Jung <rong820406@gmail.com>
2024-06-17 16:45:45 +0800

    URL change

--- src/services/course.service.js
+++ src/services/course.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const API_URL = "https://mern-project-api-bycc.onrender.com/api/courses";
-const API_URL = "http://localhost:8080/api/courses";
+const API_URL = "https://mern-project-api-bycc.onrender.com/api/courses";
+// const API_URL = "http://localhost:8080/api/courses";
 
 //HEY
 class CourseService {
